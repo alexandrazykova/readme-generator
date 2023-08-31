ad29c13a157ee102370714771dd06d4e8e397b00
alexandrazykova <alexandrazykova1@gmail.com>
2023-08-31 17:48:31 -0400

    reagganged folders

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("../Develop/utils/generateMarkdown")
 
 // an array of questions for user input
 const questions = ([
@@ -62,7 +62,7 @@ const writeToFile = (fileName, data) => {
 function init() { 
     inquirer.prompt(questions).then((response) => {
         console.log(response)
-        // TODO: Create a function to write README file
+        // function to write README file
         writeToFile("README.md", generateMarkdown(response))
     })
 }
